0b62d41e1a26b51f978b0a8212655debe2d16ce8
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2022-11-06 10:42:35 +0100

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+int();
+
+function int(){
+    
+    // Metoda log obiektu console ( wyrzuca kady typ na konsoli )
+    console.log('JavaScript');
+    console.log(7);
+    console.log(true);
+    console.log(null);
+    console.log(undefined);
+    console.log([1, 2, 3]);
+    console.log({a: 1, b: 2, c: 3});
+
+    // Metoda error wypisuje błąd w koncoli, domyślnie ta wiadomość jest koloru czerwonego
+
+    console.error( 5 );
+    console.error ( 'Błą∂∂∂∂d');
+
+    // Metoda warn wypisuje ostrzeenie w koncoli, domyślnie ta wiadomość jest koloru zółtego
+
+    console.error( 5 );
+    console.error ( 'Błą∂∂∂∂d');
+
+    // Czyszczenie konsoli, czyści i wyświetla komunikat Wyczyszczono konsolę
+    console.clear() 
+    
+}
+
